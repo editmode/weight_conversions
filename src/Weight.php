3cecc2ba7c9b22647e2a14552754e4176becbdec
Nika <ni_kush@live.com>
2025-06-08 03:02:59 +0400

    add toStones

--- src/Weight.php
+++ src/Weight.php
@@ -4,7 +4,9 @@ namespace Editmode\WeightConversions;
 
 class Weight
 {
-    public function __construct(protected float $kilograms) {}
+    public function __construct(protected float $kilograms)
+    {
+    }
 
     public static function kilograms(float $kilograms): self
     {
@@ -14,5 +16,10 @@ class Weight
     public function toPounds(): float
     {
         return $this->kilograms * 2.2046;
+    }
+
+    public function toStones()
+    {
+        return $this->kilograms * 0.15747;
     }
 }
--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -6,3 +6,8 @@ it('can convert kilograms to pounds', function () {
     $pounds = Weight::kilograms(100)->toPounds();
     expect($pounds)->toBe(220.46);
 });
+
+it('can convert kilograms to stones', function () {
+    $pounds = Weight::kilograms(100)->toStones();
+    expect($pounds)->toBe(15.747);
+});
